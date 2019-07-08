fcb0b8b4cc3348d134ded5108a56a5728f8beab6
Heather Peri <heatherhperi@gmail.com>
2019-07-08 17:37:20 -0400

    added all routes and db info

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burg', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('VegBurg', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('MexiBurg', FALSE);
